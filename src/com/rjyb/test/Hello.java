1381e3a5cc8c7b42a5a804c5104e66df343d3e7e
rjyb22 <rjyb22@163.com>
2020-12-24 08:57:09 +0800

    第一次提交

---
+++ src/com/rjyb/test/Hello.java
@@ -0,0 +1,12 @@
+package com.rjyb.test;
+
+/**
+ * @Auther: kf
+ * @Date: 2020/12/24 - 12 - 24 - 8:54
+ * @Description: com.rjyb.test
+ * @version: 1.0
+ */
+public class Hello {
+    private String name;
+    private String age;
+}
