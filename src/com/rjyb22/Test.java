25b90faf8c156d756000799f6edc7c964f01ad6b
rjyb22 <rjyb22@163.com>
2021-07-23 14:43:09 +0800

    test提交本地库

--- src/com/rjyb22/Test.java
+++ src/com/rjyb22/Test.java
@@ -6,11 +6,21 @@ public class Test {
 
     private String number;
 
+    private String passwd;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
